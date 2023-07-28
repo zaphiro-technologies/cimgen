bf56e7f1f1903d8bb42e957815bb6d484273759b
Federico M. Facca <federico.facca@zaphiro.ch>
2023-07-28 18:14:14 +0200

    uniform argument management + manage dependencies using poetry

--- build.py
+++ build.py
@@ -4,9 +4,9 @@ import argparse
 import importlib
 
 parser = argparse.ArgumentParser(description='Generate some CIM classes.')
-parser.add_argument('outdir', type=str, help='The output directory')
-parser.add_argument('schemadir', type=str, help='The schema directory')
-parser.add_argument('langdir', type=str, help='The langpack directory')
+parser.add_argument('--outdir', type=str, help='The output directory', required=True)
+parser.add_argument('--schemadir', type=str, help='The schema directory', required=True)
+parser.add_argument('--langdir', type=str, help='The langpack directory', required=True)
 parser.add_argument('--cgmes_version', type=str, choices=['cgmes_v2_4_13', 'cgmes_v2_4_15', 'cgmes_v3_0_0'], default='cgmes_v2_4_15', help='CGMES Version')
 args = parser.parse_args()
 
